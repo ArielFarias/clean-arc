eeabdf270b54681926171c84dbc24c171d70525f
Ariel Dutra Farias <ariel@ephealth.com.br>
2022-07-20 20:20:02 -0300

    feature: remove comentarios, remove pasta de teste

--- lib/clean_arc.dart
+++ lib/clean_arc.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library clean_arc;
 
 export 'src/clean_arc_cmd_runner.dart';
-
-// TODO: Export any libraries intended for clients of this package.
