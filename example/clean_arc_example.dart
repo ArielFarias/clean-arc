c454a2aaca3b7a2c1d4788fc02b5510af2d01006
Ariel Dutra Farias <ariel@ephealth.com.br>
2022-07-20 17:56:16 -0300

    feature: First commit

---
+++ example/clean_arc_example.dart
@@ -0,0 +1,6 @@
+import 'package:clean_arc/clean_arc.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/clean_arc.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library clean_arc;
+
+export 'src/clean_arc_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
